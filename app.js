be836b55b4f8bc541b49c392d91954fb855529b8
Arnaud CHESNAY <arnaud.chesnay@etu.univ-nantes.fr>
2017-09-27 09:06:27 +0200

    push website

---
+++ app.js
@@ -0,0 +1,48 @@
+'use-strict';
+
+const showdown  = require('showdown');
+const dirTree = require('directory-tree');
+const express = require('express');
+const fs = require('fs');
+
+const converter = new showdown.Converter();
+const app = express();
+const path = 'md'
+
+app.use(express.static('public'));
+
+app.get('/' + path + '/*', function(req, res) {
+    fs.readFile(req.url.substr(1), 'utf8', function(err, data) {
+        if (err)
+            return console.log(err);
+        fs.readFile('public/css/style.css', 'utf8', function(err, style) {
+            var html = '<html>';
+            html += '<head>';
+            html += '<style>' + style + '</style>';
+            html += '</head>';
+            html += '<body class=\'markdown-preview\' data-use-github-style>';
+            html += converter.makeHtml(data);
+            html += '</body>';
+            html += '</html>';
+            res.send(html);
+        });
+    });
+});
+
+app.get('/data', function(req, res) {
+    res.send(dirTree(path, {extensions:/\.md/}));
+});
+
+app.get('/', function(req, res) {
+    fs.readFile('public/index.html', 'utf8', function(err, data) {
+        if (err)
+            return console.log(err);
+        res.send(data);
+    });
+});
+
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("App listening at http://%s:%s", host, port);
+});
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $.get('/data', function(data) {
+        $('#tree').append(addDirectory(data));
+
+        $('a').click(function(e) {
+            $('.spinner').show();
+            e.preventDefault();
+            $.get(this.href, function(data) {
+                $('.spinner').hide();
+                $('#md').html(data);
+            });
+        });
+    });
+    $('.spinner').hide();
+});
+
+function addDirectory(data) {
+    var ul = document.createElement('ul');
+    data.children.forEach(function(item) {
+        var li = document.createElement('li');
+        var text = document.createTextNode(item.name);
+
+        if (item.type === "file") {
+            var a = document.createElement('a');
+            a.appendChild(text);
+            a.href = item.path;
+            li.appendChild(a);
+        } else {
+            li.appendChild(text);
+            li.appendChild(addDirectory(item));
+        }
+        ul.appendChild(li);
+    });
+    return ul;
+}
