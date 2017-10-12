4289d02e538361255cecdad6ffe286c99f966e10
ache <ache@ache.one>
2017-10-13 01:08:08 +0200

    Use userland port

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('*', function(req, res) {
     });
 });
 
-var server = app.listen(80, function() {
+var server = app.listen(8090, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("[" + new Date() + "] > " + "App listening at http://%s:%s", host, port);
